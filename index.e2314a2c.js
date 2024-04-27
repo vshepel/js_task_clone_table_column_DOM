1e50b71f5756b1f9c50d1f24b959a92a09fe909c
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-27 19:32:58 +0300

    make build

---
+++ index.e2314a2c.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),l=0;l<e.rows.length;l++){var o=e.rows[l].cells[1].cloneNode(!0),r=e.rows[l].cells.length-1;e.rows[l].cells[r].before(o)}
+//# sourceMappingURL=index.e2314a2c.js.map
